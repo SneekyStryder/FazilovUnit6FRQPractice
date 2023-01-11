631da84780a2f1405a3398dad088d8a763e542fa
Imran Fazilov <ifazilov3584@bths.edu>
2023-01-11 12:28:11 -0500

    period end

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Payroll {
+public class Payroll
+{
+    private int[] itemsSold; // number of items sold by each employee
+    private double[] wages; // wages to be computed in part (b)
+
+    public Payroll(int[] items)
+    {
+        itemsSold = items;
+        wages = new double[items.length];
+    }
+
+    public double[] getWages() {
+        return wages;
+    }
+
+    /*  Returns the bonus threshold as described in part (a).
+     */
+    public double computeBonusThreshold()
+    {
+        int most = 0;
+        int least = 100;
+        for (int num : itemsSold) {
+            if (num > most) {
+                most = num;
+            }
+            else if (num < least) {
+                least = num;
+            }
+        }
+
+        int sum = 0;
+        for (int num : itemsSold) {
+            sum += num;
+        }
+
+        return (double) (sum - most - least) / itemsSold.length - 2;
+
+    }
+
+    /* Computes employee wages as described in part (b) and stores
+    them in wages. The parameter fixedWage represents the fixed
+    amount each employee is paid per day. The parameter
+    perItemWage represents the amount each employee is paid per
+    item sold.
+    */
+    public void computeWages(double fixedWage, double perItemWage)
+    {
+        double bonusThreshold = computeBonusThreshold();
+        for (int i = 0; i < wages.length; i++) {
+            double wage = fixedWage + perItemWage * itemsSold[i];
+            if (itemsSold[i] > bonusThreshold) {
+                wage = wage * 1.1;
+            }
+            wages[i] = wage;
+        }
+
+    }
 }
--- src/Vocab.java
+++ src/Vocab.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Vocab {
+public class Vocab
+{
+    /** The controlled vocabulary for a Vocab object. */
+    private String[] theVocab;
+
+    public Vocab(String[] vocab)
+    {
+        theVocab = vocab;
+    }
+
+    /* Searches for a string in theVocab. Returns true if its String
+      parameter str is an exact match to an element in theVocab and
+      returns false otherwise.
+    */
+    public boolean findWord(String str)
+    {
+        for (String word : theVocab)
+        {
+            if (word.equals(str))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /* Counts how many strings in wordArray are not found in theVocab,
+      as described in part (a).
+    */
+    public int countNotInVocab(String[] wordArray) {
+        int counter = 0;
+        for (String word : wordArray) {
+            if (!(findWord(word))) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    /* Returns an array containing strings from wordArray not found in
+      theVocab, as described in part (b).
+    */
+    public String[] notInVocab(String[] wordArray)
+    {
+        String[] notArray = new String[countNotInVocab(wordArray)];
+        int i = 0;
+        for (String word : wordArray) {
+            if (!(findWord(word))) {
+                notArray[i] = word;
+                i += 1;
+            }
+        }
+        return notArray;
+
+    }
 }
